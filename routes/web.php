d70afb60cfdcaca7f9ca4990f6fabaf1bcb0f4d6
KhengPC <khengPC@uchiha.com>
2024-07-25 02:49:28 +0700

    make the format for category and tag Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,61 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\TagController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\TagController;
 
-
-//Display home
+// Display home
 Route::get('/', function () {
     return view('index');
 })->name('home');
 
-//Display Article
+// Display Article
 Route::get('/article', function () {
     return view('article');
 })->name('article');
 
+// Category routes
+Route::prefix('admin')->group(function () {
+    
+    // List Category
+    Route::get('category', [CategoryController::class, 'index'])->name('admin.category.index');
 
-// List Category
-Route::get('admin/category',[CategoryController::class,'index'])->name('admin.category.index');
+    // Form to create Category
+    Route::get('category/create', [CategoryController::class, 'create'])->name('admin.category.create');
 
-// Form to create Category
-Route::get('admin/category/create',[CategoryController::class,'create'])->name('admin.category.create');
+    // Form to edit Category
+    Route::get('category/{id}', [CategoryController::class, 'edit'])->name('admin.category.edit');
 
-// Form to edit Category
-Route::get('admin/category/{id}',[CategoryController::class,'edit'])->name('admin.category.edit');
+    // Form to store new Category
+    Route::post('category/store', [CategoryController::class, 'store'])->name('admin.category.store');
 
-// Form to store new Category
-Route::post('admin/category/store',[CategoryController::class,'store'])->name('admin.category.store');
+    // Form to update Category
+    Route::put('category/{id}', [CategoryController::class, 'update'])->name('admin.category.update');
 
-// Form to update Category
-Route::put('admin/category/{id}',[CategoryController::class,'update'])->name('admin.category.update');
-
-// Form to delete Category
-Route::delete('admin/category/{id}',[CategoryController::class,'destroy'])->name('admin.category.destroy');
+    // Form to delete Category
+    Route::delete('category/{id}', [CategoryController::class, 'destroy'])->name('admin.category.destroy');
+});
 
 //--------------------------------------------------
 
-//List Tag
-Route::get('admin/tag',[TagController::class,'index'])->name('admin.tag.index');
+// Tag routes
+Route::prefix('admin')->group(function () {
+    
+    // List Tag
+    Route::get('tag', [TagController::class, 'index'])->name('admin.tag.index');
 
-// Form to create Tag
-Route::get('admin/tag/create',[TagController::class,'create'])->name('admin.tag.create');
+    // Form to create Tag
+    Route::get('tag/create', [TagController::class, 'create'])->name('admin.tag.create');
 
-// Form to edit Tag
-Route::get('admin/tag/{id}',[TagController::class,'edit'])->name('admin.tag.edit');
+    // Form to edit Tag
+    Route::get('tag/{id}', [TagController::class, 'edit'])->name('admin.tag.edit');
 
-// Form to store new Tag
-Route::post('admin/tag/store',[TagController::class,'store'])->name('admin.tag.store');
+    // Form to store new Tag
+    Route::post('tag/store', [TagController::class, 'store'])->name('admin.tag.store');
 
-// Form to update Tag
-Route::put('admin/tag/{id}',[TagController::class,'update'])->name('admin.tag.update');
+    // Form to update Tag
+    Route::put('tag/{id}', [TagController::class, 'update'])->name('admin.tag.update');
 
-
-// Form to delete Tag
-Route::delete('admin/tag/{id}',[TagController::class,'destroy'])->name('admin.tag.destroy');
+    // Form to delete Tag
+    Route::delete('tag/{id}', [TagController::class, 'destroy'])->name('admin.tag.destroy');
+});
