58fa27e0dea72cb8b54f525c913dbfe6479ffc3e
KhengPC <khengPC@uchiha.com>
2024-07-27 05:28:37 +0700

    unfinish( Err $tags)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\Tag;
 
 class HomeController extends Controller
 {
     public function index(){
-        $posts = Post::paginate(8); // get all posts
-        return view('index', ['posts'=> $posts]);
+       $posts = Post::paginate(8); // get all posts
+    $categories = Category::all(); // get all categories
+    $tags = Tag::all(); // get all tags
+
+    return view('index', [
+        'posts' => $posts,
+        'nav_categories' => $categories,
+        'tags' => $tags
+    ]);
     }
     public function article(Request $request, $id){
         $post = Post::findOrFail( $id );
